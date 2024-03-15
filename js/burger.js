97c1d54f04b4d4f086ae33f20a3d25ce185399fa
berno_4444 <ivankatrenko1997@gmail.com>
2024-03-15 10:08:55 +0100

    new file

--- js/burger.js
+++ js/burger.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('header').classList.toggle('open');
     });
 })
+
+window.addEventListener('click', function (event) {
+    if (!event.target.closest('header')) {
+        document.querySelector('header').classList.remove('open');
+    }
+})
