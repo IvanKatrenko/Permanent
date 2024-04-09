cd138a9b2b9086566738d409ec2a926adb307f8f
berno_4444 <ivankatrenko1997@gmail.com>
2024-04-09 12:13:20 +0200

    js swiper

--- js/swiper.js
+++ js/swiper.js
@@ -10,7 +10,32 @@ let swiper = new Swiper(".myswiper", {
     mousewheel: true,
     keyboard: true,
     autoplay: {
-        delay: 2500,
+        delay: 3000,
+        stopOnLastSlide: false,
+        reverseDirection: false,
         disableOnInteraction: false,
+        pauseOnMouseEnter: false,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        waitForTransition: false,
+
     },
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var nextSlideButton = document.querySelector('.swiper-button-next');
+
+    nextSlideButton.addEventListener('click', function () {
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'php/swiper.php', true);
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                document.getElementsByClassName('myswiper')[0].innerHTML = xhr.responseText;
+            }
+        };
+        xhr.send();
+    })
+
+})
