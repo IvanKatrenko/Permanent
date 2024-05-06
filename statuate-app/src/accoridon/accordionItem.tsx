174e9d2e3041176065645455d5c9f5d61cf7cd79
berno_4444 <ivankatrenko1997@gmail.com>
2024-05-06 20:29:05 +0200

    permanent/statute next app

---
+++ statuate-app/src/accoridon/accordionItem.tsx
@@ -0,0 +1,13 @@
+import AccordionItem from "./AccordionItem";
+
+const Accordion = ({ items }) => {
+  return (
+    <div className="accordion">
+      {items.map((item, index) => (
+        <AccordionItem key={index} title={item.title} content={item.content} />
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
