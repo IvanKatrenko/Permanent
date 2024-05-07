816988eb38a0fa21456cd0627e469db1bef29624
berno_4444 <ivankatrenko1997@gmail.com>
2024-05-07 13:58:39 +0200

    modified statute

---
+++ statuate-app/src/app/pages/regulamin.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const Regulamin = () => {
+  return (
+    <div>
+      <Head>
+        <title>Regulamin Makijażu Permanentnego w Missis Permanent</title>
+        <meta
+          name="description"
+          content="Regulamin dotyczący makijażu permanentnego"
+        />
+      </Head>
+
+      <h1>Regulamin Makijażu Permanentnego</h1>
+      <p>Tutaj umieść tekst regulaminu dotyczącego makijażu permanentnego...</p>
+    </div>
+  );
+};
+
+export default Regulamin;
